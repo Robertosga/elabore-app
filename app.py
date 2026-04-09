5ae2819c858e43a7046c2e72888951b09210fa4f
Roberto Sillas <65824725+Robertosga@users.noreply.github.com>
2026-04-09 18:42:03 -0300

    Refactor access control and service management

--- app.py
+++ app.py
@@ -48,8 +48,8 @@ EMPRESA = {
 SERVICOS_LISTA = [
     "Toldo Policarbonato","Troca policarbonato 6mm 1ª linha","Perfil U","Borracha fina perfil", "Toldo Cortina", "Toldo Fixo Lona",
     "Toldo Retratil Braço, tubo e sarrafo aluminio","Cobertura Policarbonato",
-    "Cobertura Lona", "Painel","Painel Paraline","Fachada", "Estrutura", "Banner", "Adesivo", "Plotter",
-    "Lona", "Troca de Lona", "Placa", "Mão de Obra", "Impressão", "Calha", "Forro Paraline",
+    "Cobertura Lona", "Painel","Painel Paraline","Fachada", "Estrutura", "Banner", "Adesivo", "Plotter","Plottagem de adesivo em Painel",
+    "Lona", "Troca de Lona", "Placa", "Mão de Obra", "Impressão", "Calha", "Forro Paraline","instalação de refletor",
     "Pintura", "Cavalete", "Cartão de visita", "Panfleto 4x0, 5000 und","Panfleto 4x4, 5000 und", "Luminoso", "Logotipo",
     "Identidade Visual", "Manutenção", "Troca de Mola", "Troca de Tubo", "Vetorização","Cartaz",
     "Folder","Arte Gráfica","Site","Aplicação Webb"
